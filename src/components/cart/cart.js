b0224c812cbee379ec88b644111b62517003d2b2
cris93g <37388115+cris93g@users.noreply.github.com>
2018-07-15 14:17:55 -0500

    cart added

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -1,81 +1,56 @@
 import React, { Component } from "react";
-import { addToCart } from "../../redux/ducks/itemsReducer";
+import { readCartItems, removeFromCart,clearCart} from "../../redux/ducks/itemsReducer";
 import axios from "axios";
+import { connect } from 'react-redux';
 
 class Cart extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cart: []
-    };
+  constructor(props){
+      super(props)
+
+      this.handleClick = this.handleClick.bind(this);
   }
   componentDidMount() {
-    axios.get("/api/cart").then(res => {
-        console.log(this.state)
-      this.setState({ cart: res.data });
-    });
+      this.props.readCartItems();
   }
-  updateQuantity = (id, quantity) => {
-    if (quantity > 0) {
-      axios
-        .put("/api/update/" + id, { quantity })
-        .then(res => {
-          this.setState({ cart: res.data });
-        })
-        .catch(console.log);
-    } else {
-      axios
-        .delete(`/api/delete/${id}`)
-        .then(res => {
-          this.setState({ cart: res.data });
-        })
-        .catch(console.log);
-    }
-  };
+  handleClick() {
+      this.props.createOrder();
+      this.props.clearCart();
+  }
+  
   render() {
-    let cart = this.state.cart.map(item => {
+      console.log('cart', this.props.cart); //no touchy
       return (
-        <div key={item.id} className="prod_container">
-          <ul>
-            <li>{item.name}</li>
-            <img alt={item.name} height="50px" src={item.img_url} />
-            
-            <li>
-              <span
-                onClick={() => this.updateQuantity(item.id, item.quantity - 1)}
-              >
-                -
-              </span>
-              {item.quantity}
-              <span
-                onClick={() => this.updateQuantity(item.id, item.quantity + 1)}
-              >
-                +
-              </span>
-              {"            "}
-              {"            "}
-              <span onClick={() => this.updateQuantity(item.id, 0)}>
-                &#128465;
-              </span>{" "}
-            </li>
-            <li>${item.price}</li>
-          </ul>
-          <hr />
-        </div>
-      );
-    });
-    let total = this.state.cart.reduce(
-      (tot, cur) => tot + cur.price * cur.quantity,
-      0
-    );
-    return (
-      <div className="">
-        <div className="cart_container">
-          {cart}
-          <div className="total">Total: ${total}</div>
-        </div>
-      </div>
-    );
+          <div className="cart-app" >
+              <h2 className='title'  >Welcome To Your Cart!</h2>
+              <h3 className='sub' >Let's see what you want</h3>
+              <div className="cart-map" >
+                  {this.props.cart.map((items, i) => {
+                      return (
+                          <div key={i} className="item">
+                              <img src={items.img} alt={items.title} className='product-image' />
+                              <p>Item: #{items.item_id}</p>
+                              <p>Description: {items.name}</p>
+                              <p className="price">Product Price: ${items.price}.00</p>
+                              <button className="prod-button" onClick={() => this.props.removeFromCart(items.item_id)} >I changed my mind</button>
+                          </div>
+                      )
+                  })}
+              </div>
+              <div className= "order-button">
+                  <a href='/orders' >
+                      <button onClick={this.handleClick}>Submit order</button>
+                  </a>
+              </div>
+          </div>
+      )
   }
 }
-export default Cart;
+function mapStateToProps(state) {
+  console.log("state from Cart", state)
+  return {
+      items: state.items,
+      cart: state.cart,
+      // orders: state.orders
+  }
+}
+export default connect(mapStateToProps, { readCartItems, removeFromCart,  clearCart })(Cart);
